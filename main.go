6a93d5ea03d9027ed4651a3e3388961cc90aee40
glooms <marcus.lacerda@live.se>
2019-07-30 16:03:33 +0200

    Cleanup

--- main.go
+++ main.go
@@ -3,19 +3,12 @@ package main
 
 import (
 	_ "./decls" // See if it breakes or not
-	"./gdoc"
 	"./spec"
 	"fmt"
 	"os"
 )
 
-func GdocEx() bool {
-	gdoc.Generate("./animator")
-	return true
-}
-
 func main() {
-	//adoc.Explore("/c/Users/dxa/wsl/git/go/src/go")
 	if len(os.Args) > 1 {
 		fmt.Println(os.Args[1])
 		spec.Explore(os.Args[1])
